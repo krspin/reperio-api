2ade529745262422604e6e353c47fe1843691401
dshehbaj <shehbaj.dhillon@gmail.com>
2021-06-18 05:24:48 -0700

    Reorganization.

--- serverless.ts
+++ serverless.ts
@@ -3,14 +3,6 @@ import type { AWS } from '@serverless/typescript';
 import hello from '@functions/hello';
 import recipes from '@functions/recipes';
 
-const config = {
-  "user": "shehbaj",
-  "password": "",
-  "host": "localhost",
-  "database": "reperiodb",
-  "port": "3306"
-}
-
 const serverlessConfiguration: AWS = {
   service: 'reperio-api',
   frameworkVersion: '2',
@@ -31,11 +23,6 @@ const serverlessConfiguration: AWS = {
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      dbDatabaseName: config.database,
-      dbUsername: config.user,
-      dbPassword: config.password,
-      dbPortNumber: config.port,
-      dbHostname: config.host
     },
     lambdaHashingVersion: '20201221',
   },
--- src/database/CnnPool.ts
+++
@@ -1,9 +0,0 @@
-import { createConnection } from "mysql";
-
-export const db = createConnection({
-  host: process.env.dbHostname,
-  user: process.env.dbUsername,
-  password: process.env.dbPassword,
-  database: process.env.dbDatabaseName
-});
-
