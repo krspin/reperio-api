2d7ee0110e80c0fad10b797a8dc8bd3b3b67a16d
dshehbaj <shehbaj.dhillon@gmail.com>
2021-06-18 14:42:58 -0700

    Added name field for recipe.

--- src/sql/tables.sql
+++ src/sql/tables.sql
@@ -5,6 +5,7 @@ use reperio;
 create table user_recipes (
   id varchar(36) primary key,
   author varchar(36) not null,
+  name varchar(32) not null,
   ingredients varchar(512) not null,
   media varchar(512),
   instructions varchar(1024) not null,
