7734cd0901447f747b51bfbf7bb52f1b83acb92d
dshehbaj <shehbaj.dhillon@gmail.com>
2021-06-18 19:19:53 -0700

    Added a lambda base handler for abstraction purposes.

--- src/functions/postRecipes/handler.ts
+++ src/functions/postRecipes/handler.ts
@@ -11,29 +11,21 @@ import schema from './schema';
 const postRecipes: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
 
   let body: unknown | {[key: string]: string | number} = {};
-  let statusCode: number;
 
-  try {
-    body = await knex('user_recipes').insert({
-      id: event.body.id,
-      author: event.body.author,
-      name: event.body.name,
-      ingredients: event.body.ingredients.join(','),
-      media: event.body.media || null,
-      instructions: event.body.instructions,
-      cost: event.body.cost,
-      time: event.body.time,
-      difficulty: event.body.difficulty
-    });
-    statusCode = 200;
-  } catch (e) {
-    body = { error: e.message };
-    statusCode = 500;
-  };
+  body = await knex('user_recipes').insert({
+    id: event.body.id,
+    author: event.body.author,
+    name: event.body.name,
+    ingredients: event.body.ingredients.join(','),
+    media: event.body.media || null,
+    instructions: event.body.instructions,
+    cost: event.body.cost,
+    time: event.body.time,
+    difficulty: event.body.difficulty
+  });
 
   return formatJSONResponse({
-    body,
-    statusCode
+    body
   })
 };
 
--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,6 +1,32 @@
 import middy from "@middy/core"
 import middyJsonBodyParser from "@middy/http-json-body-parser"
 
-export const middyfy = (handler) => {
-  return middy(handler).use(middyJsonBodyParser())
+export const middyfy = (lambda) => {
+  return middy(baseHandler(lambda)).use(middyJsonBodyParser())
+}
+
+const baseHandler = (lambda) => {
+  return async function (event, context, callback) {
+    let body: unknown | {[key: string]: string | number};
+    let statusCode: number;
+
+    try {
+      body = await lambda(event, context, callback);
+      statusCode = 200;
+    } catch (e) {
+      body = { error: e.message };
+      statusCode = 500;
+    };
+
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+      /**
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true
+      }
+      **/
+    };
+  }
 }
